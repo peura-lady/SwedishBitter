411896bf089dee956533cab6ba404b6aafc8ae00
peura-lady <anna.sonninen94@gmail.com>
2021-01-03 01:14:17 +0200

    SwedishBitter Landing

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    $('.list__inner .tab, .reviews__tab').on('click', function(event) {
+        let id = $(this).attr('data-id');
+            $('.list__inner, .reviews__inner').find('.tab-item, .reviews__content-item').removeClass('active-tab').hide();
+            $('.list__inner .tabs, .reviews__tabs').find('.tab, .reviews__tab').removeClass('active');
+            $(this).addClass('active');
+            $('#'+id).addClass('active-tab').fadeIn();
+            return false;
+        });
+
+});
